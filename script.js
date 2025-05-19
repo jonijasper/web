5942ca862c0a9d8b09c19c50ccca8bf000c8917a
jonijasper <jasper.alakotila@protonmail.com>
2025-05-19 15:24:02 +0300

    another button removes text

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-const asd = document.getElementById("id1")
+const spandex = document.getElementById("id2");
 
 function dostuff() {
-    asd.innerHTML = "pöö";
+    spandex.textContent = "pöö";
 }
+
+buttonwithid = document.getElementById("button1");
+buttonwithid.addEventListener("click",undostuff);
+
+function undostuff() {
+    spandex.textContent = "";
+}
+
